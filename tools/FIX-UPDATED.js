7108c1ca00142177dce4e34a9276564c673cac5b
Frank Bennett <biercenator@gmail.com>
2020-11-25 18:43:25 +0900

    Fix logic of timestamp updater

--- tools/FIX-UPDATED.js
+++ tools/FIX-UPDATED.js
@@ -13,17 +13,25 @@ var transPath = (fn) => {
 for (let fn of fs.readdirSync(path.join(__dirname, ".."))) {
     if (fn.slice(0, 3) !== "jm-" || fn.slice(-4) !== ".csl") continue;
 
-    let mtime = fs.statSync(transPath(fn)).mtimeMs;
-    let date = new Date(mtime).toISOString().replace(/(?:\.[0-9]{3}Z)/, "Z");
+    let atime = fs.statSync(transPath(fn)).atime;
+    let mtime = fs.statSync(transPath(fn)).mtime;
+	
+    let newDate = mtime.toISOString().replace(/(?:\.[0-9]{3}Z)/, "+00:00");
 
     var txt = fs.readFileSync(transPath(fn)).toString();
     var lst = txt.split("\n");
     for (let i=0,ilen=lst.length; i<ilen; i++) {
-        let line = lst[i];
-        lst[i] = line.replace(/^(\s*<updated>).*(<\/updated>\s*)$/, `$1${date}$2`);
+        var line = lst[i];
+        var m = line.match(/^(\s*<updated>)(.*)(<\/updated>\s*)$/);
+        if (m) {
+            oldDate = m[2];
+            if (oldDate !== newDate) {
+                lst[i] = `${m[1]}${newDate}${m[3]}`;
+                txt = lst.join("\n");
+                fs.writeFileSync(transPath(fn), txt);
+				fs.utimesSync(transPath(fn), atime, mtime);
+                break;
+            }
+        }
     }
-    txt = lst.join("\n");
-    fs.writeFileSync(transPath(fn), txt);
 }
-
-     
